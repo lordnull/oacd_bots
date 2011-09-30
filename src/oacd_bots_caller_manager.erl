3286db52b08bad1a3b9723586c42f73ce54dfc06
Micah Warren <micahw@lordnull.com>
2011-09-30 11:02:05 -0500

    Connecting to freeswitch.

--- src/oacd_bots_caller_manager.erl
+++ src/oacd_bots_caller_manager.erl
@@ -19,10 +19,14 @@
 %%  Contributor(s): Micah Warren <micahw at lordnull dot com>
 
 -module(oacd_bots_caller_manager).
+-author("Micah Warren").
 -behaviour(gen_server).
 -define(SERVER, ?MODULE).
 
--record(state, {}).
+-record(state, {
+	freeswitch :: atom(),
+	check_timer :: 'undefined' | reference()
+}).
 
 %% ------------------------------------------------------------------
 %% API Function Exports
@@ -47,22 +51,64 @@ start_link(Freeswitch) ->
 start_link(Freeswitch, Options) ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, {Freeswitch, Options}, []).
 
-%% ------------------------------------------------------------------
+%% ==================================================================
 %% gen_server Function Definitions
+%% ==================================================================
+
+%% ------------------------------------------------------------------
+%% INIT
 %% ------------------------------------------------------------------
 
 init({Freeswitch, Options}) ->
-  {ok, #state{}}.
+	monitor_node(Freeswitch, true),
+	lager:info("~s started with freeswitch node ~p", [?MODULE, Freeswitch]),
+  {ok, #state{freeswitch = Freeswitch}}.
+
+%% ------------------------------------------------------------------
+%% handle_call
+%% ------------------------------------------------------------------
 
 handle_call(_Request, _From, State) ->
   {noreply, ok, State}.
 
+%% ------------------------------------------------------------------
+%% handle_cast
+%% ------------------------------------------------------------------
+
 handle_cast(_Msg, State) ->
   {noreply, State}.
 
+%% ------------------------------------------------------------------
+%% handle_info
+%% ------------------------------------------------------------------
+
+handle_info(check_freeswitch, #state{freeswitch = Fsnode} = State) ->
+	CheckTimer = case net_adm:ping(Fsnode) of
+		pang -> 
+			lager:warning("Freeswitch not available, checking again in 5 seconds"),
+			erlang:send_after(5000, self(), check_freeswitch);
+		pong -> 
+			monitor_node(Fsnode, true),
+			undefined
+	end,
+	{noreply, State#state{check_timer = CheckTimer}};
+
+handle_info({nodedown, Fsnode}, #state{freeswitch = Fsnode, check_timer = undefined} = State) ->
+	lager:warning("Freeswitch has gone down, checking in 5 seconds"),
+	CheckTimer = erlang:send_after(5000, self(), check_freeswitch),
+	{noreply, State#state{check_timer = CheckTimer}};
+
+handle_info({nodedown, Fsnode}, State) ->
+	lager:debug("Already got a node down notification, ignoring"),
+	{noreply, State};
+
+		
 handle_info(_Info, State) ->
   {noreply, State}.
 
+%% ------------------------------------------------------------------
+%% terminate
+%% ------------------------------------------------------------------
 terminate(_Reason, _State) ->
   ok.
 
